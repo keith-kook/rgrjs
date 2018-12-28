8631e50ba8ae9aa71b8a0dba81a2b0fd21a4786f
Keith Devops <keith-ko@hotmail.com>
2018-12-29 09:11:55 +1100

    Flux ready

--- server.js
+++ server.js
@@ -1,7 +1,44 @@
 import express from 'express';
-
+import {MongoClient} from 'mongodb';
 let app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000);
+
+
+
+// Connect using the MongoClient.connect static method
+ const test = require('assert');
+ // Connection url
+ const url = process.env.MONGO_URL;
+ // Database Name
+ const dbName = 'rgrjs';
+ // Connect using MongoClient
+ MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
+    if (err) throw err;
+    /* app.listen(3000, function () {
+      console.log('Server ready listen on port 3000');
+    }); */
+    app.listen(3000, ()=> console.log('Server ready listen on port 3000'));
+    const db = client.db(dbName);
+
+    app.get("/data/links", (req, res) => {
+      db.collection("links").find({}).toArray((err, links) => {
+        if (err) throw err;
+
+        res.json(links);
+      });
+    });
+
+    //client.close();
+ });
+
+/*MongoClient.connect(process.env.MONGO_URL, (err, database, useNewUrlParser: true ) => {
+  if (err) throw err;
+  database.collection("links").find({}).toArray((err, links) => {
+    if (err) throw err;
+
+    console.log(links);
+  });
+});
+*/
